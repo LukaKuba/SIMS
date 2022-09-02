3841b6974f529f98f0d78528a59e8b7c7926df9e
LukaKuba <102537825+LukaKuba@users.noreply.github.com>
2022-09-02 18:43:30 +0200

    Add files via upload

---
+++ SIMS/Lek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+public class Lek
+{
+	public string sifra
+	{ get; set; }	
+	public string ime
+	{ get; set; }
+	public string proizvodjac
+	{ get; set; }
+	public int kolicina
+	{ get; set; }
+	public bool prihvacen;
+	public bool obrisan;
+	public int cena
+	{ get; set; }
+
+
+	public Lek(string sifra, string ime, string proizvodjac, int kolicina, bool prihvacen, bool obrisan, int cena)
+    {
+		this.sifra = sifra;
+		this.ime = ime;	
+		this.proizvodjac = proizvodjac;
+		this.kolicina = kolicina;
+		this.prihvacen = prihvacen;
+		this.obrisan = obrisan;
+		this.cena = cena;
+
+    }
+
+}
---
+++ SIMS/SIMS_2022/SIMS_2022/Korisnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public enum Tip { Farmaceut, Upravnik, Lekar };
+public class Korisnik
+{
+    public string JMBG;
+    public string email;
+    public string lozinka;
+    public string ime
+    {get; set;}
+    public string prezime;
+    public string mobilni;
+    public Tip tipkorisnika;
+    public bool blokiran;
+
+    
+    public Korisnik(string ime, string prezime, string JMBG, string email, string mobilni, string lozinka, Tip tipkorisnika, bool blokiran)
+    {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.JMBG = JMBG;
+        this.email = email;
+        this.mobilni = mobilni;
+        this.lozinka = lozinka;
+        this.tipkorisnika = tipkorisnika;
+        bool bokiran = blokiran;
+    }
+
+    
+}
+
+
+
